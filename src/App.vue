1da6d450069d803a196c02cddbcb741a04d90c73
WindLeaf233 <mc.windleaf@foxmail.com>
2022-08-12 23:47:00 +0800

    :triangular_flag_on_post: impl layout

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <template>
-  <Suspense>
-    <template #default>
-      <InfoTable :loadingInstance="loadingInstance"></InfoTable>
-    </template>
-    <template #fallback></template>
-  </Suspense>
+  <div class="common-layout">
+    <el-container>
+      <el-header class="header">
+        <strong class="title">cf-info</strong>
+      </el-header>
+      <el-container>
+        <el-aside :width="asideWidth + 'px'"></el-aside>
+        <el-main>
+          <Suspense>
+            <template #default>
+              <InfoTable :loadingInstance="loadingInstance"></InfoTable>
+            </template>
+            <template #fallback></template>
+          </Suspense>
+        </el-main>
+        <el-aside :width="asideWidth + 'px'"></el-aside>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script setup>
 import { defineAsyncComponent } from 'vue'
 import { ElLoading } from 'element-plus'
 
+const asideWidth = '150'
 const loadingInstance = ElLoading.service({ fullscreen: true, lock: true })
+
 const InfoTable = defineAsyncComponent(() => import('@/components/InfoTable'))
 </script>
+
+<style scoped>
+.header {
+  margin: auto;
+  padding: 3% 3% 5%;
+}
+
+.title {
+  font-size: 30px;
+  margin: auto;
+}
+</style>
