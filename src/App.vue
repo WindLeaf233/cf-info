0d027129c2e33d9f13ec675bafbafa6ac8f81df9
WindLeaf233 <mc.windleaf@foxmail.com>
2022-08-12 23:22:32 +0800

    :triangular_flag_on_post: impl async request data

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,16 @@
 <template>
-  <el-table
-      :data="data"
-      style="width: 100%"
-      :default-sort="{ prop: 'time', order: 'descending' }"
-  >
-    <el-table-column prop="line" label="节点" sortable></el-table-column>
-    <el-table-column prop="ip" label="IP 地址" sortable></el-table-column>
-    <el-table-column prop="latency" label="平均延迟" sortable :formatter="formatterLatency"></el-table-column>
-    <el-table-column prop="loss" label="丢包率" sortable :formatter="formatterLoss"></el-table-column>
-    <el-table-column prop="speed" label="下载速度" sortable :formatter="formatterSpeed"></el-table-column>
-    <el-table-column prop="colo" label="机房" sortable></el-table-column>
-    <el-table-column prop="sptname" label="测速节点" sortable></el-table-column>
-    <el-table-column prop="time" label="时间" sortable></el-table-column>
-  </el-table>
+  <Suspense>
+    <template #default>
+      <InfoTable></InfoTable>
+    </template>
+    <template #fallback>
+      Loading...
+    </template>
+  </Suspense>
 </template>
 
 <script setup>
-const formatterLatency = (row) => `${row.latency}ms`
-const formatterLoss = (row) => `${row.loss}%`
-const formatterSpeed = (row) => `${row.speed}MB/s`
+import { defineAsyncComponent } from 'vue'
 
-const data = [
-  {
-    colo: "美国",
-    ip: "104.20.117.36",
-    latency: 206,
-    line: "CM",
-    loss: 0,
-    speed: 425,
-    time: "2022-08-10 21:08:49",
-    sptname: "广东东苑"
-  },
-  {
-    colo: "中国",
-    ip: "114.51.41.91",
-    latency: 13,
-    line: "CT",
-    loss: 0,
-    speed: 999,
-    time: "2022-08-10 23:04:12",
-    sptname: "上海"
-  }
-];
+const InfoTable = defineAsyncComponent(() => import('@/components/InfoTable'))
 </script>
---
+++ src/components/InfoTable.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-table :data="data" style="width: 100%" :default-sort="{ prop: 'time', order: 'descending' }">
+    <el-table-column prop="line" label="节点" sortable></el-table-column>
+    <el-table-column prop="ip" label="IP 地址" sortable></el-table-column>
+    <el-table-column prop="latency" label="平均延迟" sortable :formatter="formatterLatency"></el-table-column>
+    <el-table-column prop="loss" label="丢包率" sortable :formatter="formatterLoss"></el-table-column>
+    <el-table-column prop="speed" label="下载速度" sortable :formatter="formatterSpeed"></el-table-column>
+    <el-table-column prop="colo" label="机房" sortable></el-table-column>
+    <el-table-column prop="sptname" label="测速节点" sortable></el-table-column>
+    <el-table-column prop="time" label="时间" sortable></el-table-column>
+  </el-table>
+</template>
+
+<script setup>
+import axios from 'axios'
+import { ElNotification } from 'element-plus'
+
+const formatterLatency = (row) => `${row.latency}ms`
+const formatterLoss = (row) => `${row.loss}%`
+const formatterSpeed = (row) => `${row.speed}MB/s`
+
+const getData = async function() {
+  return new Promise((resolve) =>
+    axios.get(process.env.VUE_APP_API).then((res) => resolve(res.data.data.server)).catch((err) => {
+      console.log(err)
+      ElNotification({
+        title: '发生了错误',
+        message: err.toString(),
+        type: 'error'
+      })
+    }))
+}
+
+let data = await getData()
+</script>
